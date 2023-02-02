df208302305db1cee695575eb4fac1834c3462be
Nghia Lam <nghialam12795@gmail.com>
2023-02-02 23:07:57 +0700

    Update GameMode

---
+++ Source/CGSpectrumCollab/Public/Core/CGSGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CGSGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CGSPECTRUMCOLLAB_API ACGSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
